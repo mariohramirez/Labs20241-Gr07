3d8a137c61c93c9e4a57953964ad89f4b8bdf53d
Mario Ramírez <marioh.ramirez@udea.edu.co>
2024-04-29 01:54:12 -0500

    Lab1 first commit

---
+++ Lab2-UI/src/main/java/co/edu/udea/compumovil/gr07_20241/lab2/MainActivity.kt
@@ -0,0 +1,46 @@
+package co.edu.udea.compumovil.gr07_20241.lab2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.udea.compumovil.gr07_20241.lab2.ui.theme.Labs20241Gr07Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Labs20241Gr07Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Labs20241Gr07Theme {
+        Greeting("Android")
+    }
+}
